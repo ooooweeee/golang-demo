18172ebc5de509f431ac0601c497a47154fd1027
ooooweeee <kirby18994929493@icloud.com>
2021-09-06 00:20:56 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# Golang
+
+> `golang` 开发
+
+- 初始化项目
+
+```
+go mod init example.com
+```
+
+- 安装/清理项目中的依赖包
+
+```
+go mod tidy
+```
---
+++ main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+
+	"example.com/morestrings"
+	"github.com/google/go-cmp/cmp"
+)
+
+func main() {
+	fmt.Println(morestrings.ReverseRunes("!oG ,olleH"))
+	fmt.Println(cmp.Diff("Hello World", "Hello Go"))
+}
